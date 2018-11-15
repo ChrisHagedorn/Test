7dc60416667c8f54262974f76f8a3a59d93ba55b
Chris Hagedorn <hagedornc@ismanila.org>
2018-11-15 09:58:53 +0800

    IntelJ

---
+++ src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    public Button btnButton;
+    public Label lblLabel;
+    private Model model = new Model();
+
+    public void handleButtonClick(ActionEvent actionEvent) {
+        model.increment();
+        lblLabel.setText(String.valueOf(model.getCount()));
+
+    }
+}
